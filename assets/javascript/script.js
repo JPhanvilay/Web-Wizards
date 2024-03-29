8a8066ff1a3deafa4698eadad5809ff2d745f601
Brenda Haight <brendaaureliano9@gmail.com>
2024-03-28 19:19:26 -0500

    adding the navbar

---
+++ assets/javascript/script.js
@@ -0,0 +1,51 @@
+document.getElementById("sidebarIcon").addEventListener("click", function () {
+  let dropdown = document.getElementById("dropdownMenu");
+  if (dropdown.classList.contains("hidden")) {
+    dropdown.classList.remove("hidden");
+  } else {
+    dropdown.classList.add("hidden");
+  }
+});
+
+const questionElement = document.getElementById("question");
+const questionText = "Which house do you belong to?";
+
+function animateText() {
+  let index = 0;
+  const intervalId = setInterval(() => {
+    questionElement.textContent = questionText.slice(0, index);
+    index++;
+    if (index > questionText.length) {
+      clearInterval(intervalId);
+    }
+  }, 100);
+}
+
+function checkHouse() {
+  const houseInput = document
+    .getElementById("houseInput")
+    .value.trim()
+    .toLowerCase();
+
+  const questionContainer = document.getElementById("questionContainer");
+
+  if (
+    houseInput === "gryffindor" ||
+    houseInput === "hufflepuff" ||
+    houseInput === "ravenclaw" ||
+    houseInput === "slytherin"
+  ) {
+    const backgroundImageURL = `url("images/${houseInput}.jpeg")`;
+    document.body.style.backgroundImage = backgroundImageURL;
+    questionContainer.style.display = "none";
+  } else {
+    document.getElementById("sortingHatSuggestion").innerText =
+      "If you're sure, try the Sorting Hat!";
+
+    document.getElementById("sortingHatSuggestion").classList.remove("hidden");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  animateText();
+});
